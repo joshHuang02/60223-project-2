fd02fe51e8299a52ecc9195dbd343d5f50e1dc37
joshHuang02 <52260865+joshHuang02@users.noreply.github.com>
2023-10-22 16:41:37 -0400

    clean up code

--- project-2-main/project-2-main.ino
+++ project-2-main/project-2-main.ino
@@ -12,22 +12,24 @@ Servo servo;
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 
-// vars
-int handleDistLimit = 110;
+// limiter values
+int handleDistLimit = 10;
 int rfidTimeoutMS = 2000;
 int servoDefault = 150;
 int servoTimeoutMS = 5000;
 
+// vars
 unsigned long rfidScanTime = 0;
 unsigned long servoTime = 0;
 
-bool rfid = false;
-bool handleTurned = false;
-bool doorOpen = false;
-bool doorBlocked = false;
-
+// status booleans
+bool rfid;
+bool handleTurned;
+bool doorOpen;
+bool doorBlocked;
 bool servoTriggered;
 
+// most lines in setup are taken from example code
 void setup() {
   Serial.begin(115200);
 
@@ -37,7 +39,7 @@ void setup() {
   servo.attach(SERVO);
   servo.write(servoDefault);
 
-  // Lazer 
+  // laser 
   // wait until serial port opens for native USB devices
   while (! Serial) {
     delay(1);
@@ -55,62 +57,46 @@ void setup() {
   while (!Serial);		// Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
 	SPI.begin();			// Init SPI bus
 	mfrc522.PCD_Init();		// Init MFRC522
-	delay(4);				// Optional delay. Some board do need more time after init to be ready, see Readme
+	delay(4);				// Optional delay. Some board do need more time after init to be ready
 	mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
 	Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
 
 void loop() {
-  // delay(10);
-
   // read inputs and set conditonal booleans
-  readLazer(); // reads lazer distance measurement and sets handleTurned bool
+  readLaser(); // reads laser distance measurement and sets handleTurned bool
   readRFID(); // scans for rfid tag and sets rfid bool
   doorOpen = !digitalRead(DOOR_PIN);
   bool servoTimedOut = millis() > servoTime + servoTimeoutMS;
 
-  // print status
   printStatus();
 
+  // control blocker
   if (!doorBlocked) { // blocked down
     if (handleTurned && !doorOpen && !rfid) {
       servoTime = millis();
-      servo.write(servoDefault - 100);
+      servo.write(servoDefault - 80); // lift blocker
       doorBlocked = true;
     }
   } else { //blocker up
     if ((servoTimedOut && !handleTurned) || rfid || doorOpen) {
-      servo.write(servoDefault);
+      servo.write(servoDefault); // drop blocker
       doorBlocked = false;
     }
   }
-
-  // if (handleTurned && !doorOpen && !rfidPresent && !servoTriggered && !servoTimedOut) {
-  //   // servoTriggered = true;
-  //   servoTime = millis();
-
-  //   servo.write(servoDefault + 90);
-  // } else {
-  //   servo.write(servoDefault);
-  // }
 }
 
-void readLazer() {
-  // lazer
+// reads the laser distance to flag on door handle, update the handleTurned boolean 
+void readLaser() {
   VL53L0X_RangingMeasurementData_t measure;
     
-  // Serial.print("Reading a measurement... ");
   lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
 
   if (measure.RangeStatus != 4) {  // phase failures have incorrect data
     // Serial.print("Distance (mm): "); Serial.println(measure.RangeMilliMeter);
     handleTurned = measure.RangeMilliMeter > handleDistLimit;
-  } else {
-    // Serial.println(" out of range ");
   }
-    
-  // delay(100);
 }
 
 void readRFID() {
@@ -119,24 +105,19 @@ void readRFID() {
   rfid = millis() < rfidTimeoutMS + rfidScanTime;
 
   // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-	if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    // Serial.println("Not Present");
-		return;
-	}
+	if ( ! mfrc522.PICC_IsNewCardPresent()) return;
 
-  // Serial.println("Present");
+  // At this point there is a RFID device present
   rfidScanTime = millis();
 	// Select one of the cards
 	if ( ! mfrc522.PICC_ReadCardSerial()) {
 		return;
 	}
-
-	// Dump debug info about the card; PICC_HaltA() is automatically called
-	// mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 }
 
+// debugging status of each input
 void printStatus() {
-  Serial.println("Handle Turned: ");
+  Serial.print("Handle Turned: ");
   Serial.print(handleTurned);
   Serial.print(" | RFID: ");
   Serial.print(rfid);
